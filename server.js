6451a219a03776bcc1b52b1bd01bfbf1d007610c
Sille Van Landschoot <info@sillevl.be>
2016-12-06 20:21:57 +0100

    change rethinkdb to http API

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 'use strict';
 
 var ttn = require('ttn');
-var r = require('rethinkdb');
 var settings = require('./settings.json');
+var request = require('request');
 
 var client = new ttn.Client('staging.thethingsnetwork.org', settings.appEUI, settings.accessKey);
 
-var connection = null;
-r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-
-    // r.db('test').tableCreate('featherweather').run(connection, function(err, result) {
-    //     if (err) throw err;
-    //     console.log(JSON.stringify(result, null, 2));
-    // });
-});
 
 client.on('connect', function () {
 	console.log('[DEBUG]', 'Connected');
@@ -31,21 +21,18 @@ client.on('activation', function (e) {
 
 client.on('uplink', function (msg) {
 	console.info('[INFO] ', 'Uplink: ' + JSON.stringify(msg, null, 2));
-    r.table("featherweather").insert(
-        msg
-    ).run(connection, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-    })
+	var options = {
+				url: 'http://localhost:4005/api/coordinate',
+				method: 'POST',
+				json: true,
+				body: {coordinate: msg}
+	}
+	request(options, function(err,res,body){
+					console.log('status: ' + res.statusCode);
+	});
 });
 
 client.on('uplink', function (msg) {
 
-	// respond to every third message
-	if (msg.counter % 3 === 0) {
-		console.log('[DEBUG]', 'Downlink');
 
-		var payload = new Buffer('4869', 'hex');
-		client.downlink(msg.devEUI, payload);
-	}
 });
